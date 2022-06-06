d067af5042c405691318e1124b326d0f81071432
soetomo12 <99224837+soetomo12@users.noreply.github.com>
2022-06-06 11:20:13 +0700

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,7 +8,8 @@ import { faGoogle } from "@fortawesome/free-brands-svg-icons";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import "../style.css";
-const { REACT_APP_API_LOGIN, REACT_APP_API_OAUTH } = process.env;
+
+const { REACT_APP_API_OAUTH, REACT_APP_API_LOGIN } = process.env;
 
 const Login = (props) => {
   const navigate = useNavigate();
@@ -90,67 +91,88 @@ const Login = (props) => {
 
   return (
     <>
-      <Header />
-      <div>
-        <header className="App-header container section-mb">
-          <Container className="card card-login mx-auto mt-5 pr-5 pl-5" style={{ paddingleft: "50px" }}>
-            <Row className="card-header">
-              <Col className="card-body">
-                {!token ? (
-                  <>
-                    <Button variant="light" href="/">
-                      <img src="images/Home/img_back2.png" style={{ width: "50px" }} href="/"></img>
-                    </Button>
-                    <div className="d-grid">
-                      <div className="m-auto">
-                        <Button variant="primary" onClick={() => login()}>
-                          <FontAwesomeIcon icon={faGoogle} /> Sign in with Google
-                        </Button>
-                      </div>
-                    </div>
-
-                    <Form className="mt-5 form-group " onSubmit={handleSubmit}>
-                      <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label style={{ paddingleft: "50px" }}>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)} required />
-                        <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
-                      </Form.Group>
-
-                      <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Label style={{ paddingleft: "50px" }}>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
-                      </Form.Group>
-
-                      <div className="d-grid gap-2">
-                        <Button variant="primary" size="lg" type="submit">
-                          Submit
-                        </Button>
-                      </div>
-                      <div className="text-center">
-                        <a className="d-block small mt-3" href="/register">
-                          Register Your Account
-                        </a>
-                      </div>
-                    </Form>
-                  </>
-                ) : (
-                  <>
-                    <Button variant="light" href="/">
-                      <img src="images/Home/img_back2.png" style={{ width: "50px" }} href="/"></img>
-                    </Button>
-                    <div className="d-grid gap-2">
-                      <Button variant="danger" size="lg" onClick={handleLogout}>
-                        Logout
+    <Header />
+    <div>
+      <header className="App-header container section-mb">
+        <Container
+          className="card card-login mx-auto mt-5 pr-5 pl-5"
+          style={{ paddingleft: "50px" }}
+        >
+          <Row className="card-header">
+            <Col className="card-body">
+              {!token ? (
+                <>
+                <Button variant="light" href="/">
+                <img src="images/Home/img_back2.png" style={{width: "50px"}} href="/"></img>
+                </Button>
+                  <div className="d-grid">
+                    <div className="m-auto">
+                      <Button variant="primary" onClick={() => login()}>
+                        <FontAwesomeIcon icon={faGoogle} /> Sign in with Google
                       </Button>
                     </div>
-                  </>
-                )}
-              </Col>
-            </Row>
-          </Container>
-        </header>
-      </div>
-      <Footer />
+                  </div>
+
+                  <Form className="mt-5 form-group " onSubmit={handleSubmit}>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                      <Form.Label style={{ paddingleft: "50px" }}>
+                        Email address
+                      </Form.Label>
+                      <Form.Control
+                        type="email"
+                        placeholder="Enter email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                      />
+                      <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                      </Form.Text>
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                      <Form.Label style={{ paddingleft: "50px" }}>
+                        Password
+                      </Form.Label>
+                      <Form.Control
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                      />
+                    </Form.Group>
+
+                    <div className="d-grid gap-2">
+                      <Button variant="primary" size="lg" type="submit">
+                        Submit
+                      </Button>
+                    </div>
+                    <div className="text-center">
+                      <a className="d-block small mt-3" href="/register">
+                        Register Your Account
+                      </a>
+                    </div>
+                  </Form>
+                </>
+              ) : (
+                <>
+                <Button variant="light" href="/">
+                <img src="images/Home/img_back2.png" style={{width: "50px"}} href="/"></img>
+                </Button>
+                <div className="d-grid gap-2">
+                  <Button variant="danger" size="lg" onClick={handleLogout}>
+                    Logout
+                  </Button>
+                </div>
+                </>
+              )}
+            </Col>
+          </Row>
+        </Container>
+      </header>
+    </div>
+    <Footer />
     </>
   );
 };
